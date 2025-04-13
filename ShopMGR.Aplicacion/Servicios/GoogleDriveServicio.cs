baf6507d0cb582f37daa8df54e47bf9eb9588d7b
Tuqui77 <tomasmerino58@gmail.com>
2025-04-13 15:00:20 -0300

    Subida de archivos a Drive funcional. Falta integrar la subida de archivos con el endpoint que guarda las fotos de un trabajo.

---
+++ ShopMGR.Aplicacion/Servicios/GoogleDriveServicio.cs
@@ -0,0 +1,38 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Org.BouncyCastle.Asn1.Crmf;
+using ShopMGR.Aplicacion.Interfaces;
+using ShopMGR.Infraestructura.Drive;
+
+namespace ShopMGR.Aplicacion.Servicios
+{
+    public class GoogleDriveServicio : IGoogleDriveServicio
+    {
+        private readonly GoogleDriveClient _client;
+        private readonly GoogleDriveSettings _settings;
+
+        public GoogleDriveServicio(GoogleDriveClient client, IOptions<GoogleDriveSettings> options)
+        {
+            _client = client;
+            _settings = options.Value;
+        }
+
+        public async Task<UserCredential> ConectarConGoogleDrive()
+        {
+            var credentials = await _client.LoginAsync(_settings.ClientId, _settings.ClientSecret);
+
+            return credentials;
+        }
+
+        public async Task SubirArchivoAsync(IFormFile archivos)
+        {
+            await _client.LoginAsync(_settings.ClientId, _settings.ClientSecret);
+
+            using var stream = archivos.OpenReadStream();
+
+            await _client.SubirArchivoAsync(stream, archivos.FileName, archivos.ContentType);
+        }
+    }
+}
