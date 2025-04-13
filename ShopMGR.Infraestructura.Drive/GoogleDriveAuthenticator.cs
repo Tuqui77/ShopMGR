81d90d5e6ed6ab1cf564a05eabfd8b7054c43217
Tuqui77 <tomasmerino58@gmail.com>
2025-04-13 00:02:58 -0300

    Modificación de la estructura de la aplicación. Comienzo de la implementación de Google Drive.

---
+++ ShopMGR.Infraestructura.Drive/GoogleDriveAuthenticator.cs
@@ -0,0 +1,26 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Util.Store;
+
+namespace ShopMGR.Infraestructura.Drive
+{
+    public class GoogleDriveAuthenticator
+    {
+        public async Task<UserCredential> LoginAsync(string clientId, string clientSecret)
+        {
+            var secrets = new ClientSecrets()
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            };
+
+            return await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                secrets,
+                new[] { DriveService.Scope.Drive },
+                "usuario",
+                CancellationToken.None,
+                new FileDataStore("TokenStorage"));
+
+        }
+    }
+}
---
+++ ShopMGR.Infraestructura.Drive/GoogleDriveSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMGR.Infraestructura.Drive
+{
+    public class GoogleDriveSettings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
