21ee4f4faebc90df5e7dd46c5a2c0026e01267c3
Tuqui77 <tomasmerino58@gmail.com>
2025-04-17 13:08:44 -0300

    Agregada entidad para configuraciones.
    Migración para dbo.Configuraciones.
    Seeder arreglado para insertar ValorHoraDeTrabajo.

---
+++ ShopMGR.Contexto/Migrations/20250417154605_AddConfiguraconGlobal.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopMGR.Contexto.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConfiguraconGlobal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Configuraciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Clave = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Valor = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Configuraciones", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Configuraciones");
+        }
+    }
+}
