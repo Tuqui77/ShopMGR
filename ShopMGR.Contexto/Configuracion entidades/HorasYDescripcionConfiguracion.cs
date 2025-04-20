1c16a0b93a0121d5a1663bee0774e0db3ba51f2f
Tuqui77 <tomasmerino58@gmail.com>
2025-04-20 20:05:24 -0300

    HorasYDescripcionConfiguracion
    Clase de configuracion creada, agregado el DbSet al contexto y migración añadida.

---
+++ ShopMGR.Contexto/Configuracion entidades/HorasYDescripcionConfiguracion.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopMGR.Dominio.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMGR.Contexto.Configuracion_entidades
+{
+    public class HorasYDescripcionConfiguracion : IEntityTypeConfiguration<HorasYDescripcion>
+    {
+        public void Configure(EntityTypeBuilder<HorasYDescripcion> builder)
+        {
+            builder.Property(h => h.Id)
+                .IsRequired(true);
+            builder.HasIndex(h => h.Id)
+                .IsUnique(true);
+
+            builder.Property(h => h.Horas)
+                .IsRequired(true);
+
+            builder.Property(h => h.Descripcion)
+                .IsRequired(true)
+                .HasMaxLength(500);
+
+            builder.Property(h => h.Fecha)
+                .IsRequired(true)
+                .HasColumnType("datetime");
+
+            //Relaciones
+            builder.HasOne(h => h.Trabajo)
+                .WithMany(t => t.HorasDeTrabajo)
+                .HasForeignKey(h => h.IdTrabajo)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
