16222f24a34b1a16d4532e721482657f1e484610
Tuqui77 <tomasmerino58@gmail.com>
2025-04-20 01:38:28 -0300

    Configuracion TelefonoCliente separada del contexto
    Clase TelefonoConfiguracion creada, migraciones añadidas

---
+++ ShopMGR.Contexto/Configuracion entidades/TelefonoConfiguracion.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopMGR.Dominio.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMGR.Contexto.Configuracion_entidades
+{
+    public class TelefonoConfiguracion : IEntityTypeConfiguration<TelefonoCliente>
+    {
+        public void Configure(EntityTypeBuilder<TelefonoCliente> builder)
+        {
+            builder.Property(t => t.Id)
+                .IsRequired(true);
+            builder.HasIndex(t => t.Id)
+                .IsUnique(true);
+
+            builder.Property(t => t.Telefono)
+                .IsRequired(true)
+                .HasMaxLength(15);
+
+            builder.Property(t => t.Descripcion)
+                .IsRequired(false)
+                .HasMaxLength(50);
+
+            //Relaciones
+            builder.HasOne(t => t.Cliente)
+                .WithMany(c => c.Telefono)
+                .HasForeignKey(t => t.IdCliente)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
