59a2e3810edc88366c9e447d70194344e8317d51
Tuqui77 <tomasmerino58@gmail.com>
2025-04-24 22:25:09 -0300

    Update Trabajo.cs
    Agregada la propiedad para llevar la cuenta de las horas de trabajo.

--- ShopMGR.Dominio/Modelo/Trabajo.cs
+++ ShopMGR.Dominio/Modelo/Trabajo.cs
@@ -1,14 +1,16 @@
-﻿using ShopMGR.Dominio.Enums;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using ShopMGR.Dominio.Enums;
 
 namespace ShopMGR.Dominio.Modelo
 {
     public class Trabajo
     {
         public int Id { get; set; }
-
         public EstadoTrabajo Estado { get; set; }
         public DateTime? FechaInicio { get; set; }
         public string Titulo { get; set; }
+        [NotMapped] 
+        public float TotalHoras => HorasDeTrabajo.Sum(h => h.Horas);
 
         //Relaciones
         public List<Foto> Fotos { get; set; } = [];
