75198b309b7ea037c1678d255fc42ebf232b9a1c
Tuqui <tomasmerino58@gmail.com>
2025-10-19 15:57:41 -0300

    fix: DbSet Usuario y migracion faltante

---
+++ ShopMGR.Contexto/Migrations/20251019183654_UsuarioDbSet.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopMGR.Contexto.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsuarioDbSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuario",
+                table: "Usuario");
+
+            migrationBuilder.RenameTable(
+                name: "Usuario",
+                newName: "Usuarios");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Usuario_Id",
+                table: "Usuarios",
+                newName: "IX_Usuarios_Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios");
+
+            migrationBuilder.RenameTable(
+                name: "Usuarios",
+                newName: "Usuario");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Usuarios_Id",
+                table: "Usuario",
+                newName: "IX_Usuario_Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuario",
+                table: "Usuario",
+                column: "Id");
+        }
+    }
+}
