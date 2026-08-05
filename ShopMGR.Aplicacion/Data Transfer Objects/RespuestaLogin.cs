05df8a322b8fb09acf485a8dea4c794252454dcc
Tuqui <tomasmerino58@gmail.com>
2026-08-04 22:54:21 -0300

    feat(auth): agregar RequiereCambioContraseña a RespuestaLogin y DTO ResumenUsuarios
    Refs #99

--- ShopMGR.Aplicacion/Data Transfer Objects/RespuestaLogin.cs
+++ ShopMGR.Aplicacion/Data Transfer Objects/RespuestaLogin.cs
@@ -8,10 +8,12 @@ public class RespuestaLogin
     public string AccessToken { get; set; } = null!;
     [JsonIgnore]
     public string RefreshToken { get; set; } = null!;
+    public bool RequiereCambioContraseña { get; set; } = false;
 
-    public RespuestaLogin(string accessToken, string refreshToken)
+    public RespuestaLogin(string accessToken, string refreshToken, bool requiereCambioContraseña)
     {
         AccessToken = accessToken;
         RefreshToken = refreshToken;
+        RequiereCambioContraseña = requiereCambioContraseña;
     }
 }
